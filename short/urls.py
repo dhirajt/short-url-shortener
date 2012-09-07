4c870dc582f06bae0f4f577c348363c719263a1d
Dhiraj <dhirajthakur92@facebook.com>
2012-09-07 15:49:13 +0530

    Updated names of URLs in urls.py

--- short/urls.py
+++ short/urls.py
@@ -13,8 +13,8 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-     url(r'^admin/', include(admin.site.urls)),
+     url(r'^admin/', include(admin.site.urls),name='admin'),
 
-     url(r'^(.....)/','short.views.redirect'),
+     url(r'^(.....)/','short.views.redirect',name='redirect'),
      
 )
